0596c7194dd9c78d7f8d2ced55720acfa25f70cf
sn0wmanmj <sn0wmanmj@users.noreply.github.com>
2019-09-10 20:25:39 +0200

    added INT to ids

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,14 +1,14 @@
 PRAGMA foreign_keys = ON;
 
 CREATE TABLE fachbereich (
-    id PRIMARY KEY AUTOINCREMENT,
+    id INT PRIMARY KEY AUTOINCREMENT,
     name TEXT,
     alt TEXT,
     abkuerzung TEXT
 );
 
 CREATE TABLE nutzer (
-    id PRIMARY KEY AUTOINCREMENT,
+    id INT PRIMARY KEY AUTOINCREMENT,
     nickname TEXT UNIQUE,
     vorname TEXT,
     nachname TEXT,
@@ -19,51 +19,51 @@ CREATE TABLE nutzer (
 );
 
 CREATE TABLE foto (
-    id PRIMARY KEY AUTOINCREMENT,
+    id INT PRIMARY KEY AUTOINCREMENT,
     uri TEXT,
     visible BOOL
 );
 
 CREATE TABLE datei (
-    id PRIMARY KEY AUTOINCREMENT,
+    id INT PRIMARY KEY AUTOINCREMENT,
     uri TEXT,
     visible BOOL
 );
 
 CREATE TABLE tag (
-    id PRIMARY KEY AUTOINCREMENT,
+    id INT PRIMARY KEY AUTOINCREMENT,
     name TEXT,
     visible BOOL
 );
 
 CREATE TABLE gebaeude (
-    id PRIMARY KEY AUTOINCREMENT,
+    id INT PRIMARY KEY AUTOINCREMENT,
     name TEXT
 );
 
 CREATE TABLE raum (
-    id PRIMARY KEY AUTOINCREMENT,
+    id INT PRIMARY KEY AUTOINCREMENT,
     name TEXT,
     gebaeude_id INT,
     FOREIGN KEY (gebaeude_id) REFERENCES gebaeude(id)
 );
 
 CREATE TABLE moebel (
-    id PRIMARY KEY AUTOINCREMENT,
+    id INT PRIMARY KEY AUTOINCREMENT,
     name TEXT,
     raum_id INT,
     FOREIGN KEY (raum_id) REFERENCES raum(id)
 );
 
 CREATE TABLE ort (
-    id PRIMARY KEY AUTOINCREMENT,
+    id INT PRIMARY KEY AUTOINCREMENT,
     name TEXT,
     moebel_id INT,
     FOREIGN KEY (moebel_id) REFERENCES moebel(id)
 );
 
 CREATE TABLE gegenstand (
-    id PRIMARY KEY AUTOINCREMENT,
+    id INT PRIMARY KEY AUTOINCREMENT,
     name TEXT,
     notes TEXT,
     foto_id INT,
