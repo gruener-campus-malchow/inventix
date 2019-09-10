3edb85212c06d6831b31e6e431581de9b4163877
sn0wmanmj <sn0wmanmj@users.noreply.github.com>
2019-09-10 20:24:43 +0200

    added parentheses to foreign keys

--- sql/setup.sql
+++ sql/setup.sql
@@ -15,7 +15,7 @@ CREATE TABLE nutzer (
     email TEXT,
     password TEXT,
     fachbereich_id INT,
-    FOREIGN KEY fachbereich_id REFERENCES fachbereich(id)
+    FOREIGN KEY (fachbereich_id) REFERENCES fachbereich(id)
 );
 
 CREATE TABLE foto (
@@ -45,21 +45,21 @@ CREATE TABLE raum (
     id PRIMARY KEY AUTOINCREMENT,
     name TEXT,
     gebaeude_id INT,
-    FOREIGN KEY gebaeude_id REFERENCES gebaeude(id)
+    FOREIGN KEY (gebaeude_id) REFERENCES gebaeude(id)
 );
 
 CREATE TABLE moebel (
     id PRIMARY KEY AUTOINCREMENT,
     name TEXT,
     raum_id INT,
-    FOREIGN KEY raum_id REFERENCES raum(id)
+    FOREIGN KEY (raum_id) REFERENCES raum(id)
 );
 
 CREATE TABLE ort (
     id PRIMARY KEY AUTOINCREMENT,
     name TEXT,
     moebel_id INT,
-    FOREIGN KEY moebel_id REFERENCES moebel(id)
+    FOREIGN KEY (moebel_id) REFERENCES moebel(id)
 );
 
 CREATE TABLE gegenstand (
@@ -70,14 +70,14 @@ CREATE TABLE gegenstand (
     ort_id INT,
     nutzer_id INT,
     visible BOOL,
-    FOREIGN KEY foto_id REFERENCES foto(id),
-    FOREIGN KEY ort_id REFERENCES ort(id),
-    FOREIGN KEY nutzer_id REFERENCES nutzer(id)
+    FOREIGN KEY (foto_id) REFERENCES foto(id),
+    FOREIGN KEY (ort_id) REFERENCES ort(id),
+    FOREIGN KEY (nutzer_id) REFERENCES nutzer(id)
 );
 
 CREATE TABLE datei_gegenstand (
     datei_id INT,
     gegenstand_id INT,
-    FOREIGN KEY datei_id REFERENCES datei(id),
-    FOREIGN KEY gegenstand_id REFERENCES gegenstand(id)
+    FOREIGN KEY (datei_id) REFERENCES datei(id),
+    FOREIGN KEY (gegenstand_id) REFERENCES gegenstand(id)
 )
