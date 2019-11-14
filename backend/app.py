5b3a7a67b5f51ec525fadbb59b22092bbd64efa8
sn0wmanmj <sn0wmanmj@users.noreply.github.com>
2019-11-14 12:26:51 +0100

    added rudimentary getItem

--- backend/app.py
+++ backend/app.py
@@ -46,3 +46,9 @@ async def search_items(name: str = "", notes: str = ""):
         items.append({"id": item["id"], "name": item["name"], "notes": item["notes"]})
 
     return items
+
+
+@app.get("/getItem/{item_id}")
+async def get_item(item_id: int):
+    c.execute("""SELECT * FROM gegenstand WHERE id = ?""", (item_id,))
+    return c.fetchone()
