da7f43b12cdc7d5208e9580dc6d78ed0f360c22f
Alexander Baldauf <baldauf@gruener-campus-malchow.de>
2022-11-25 15:10:54 +0100

    added weekly scrum

---
+++ api/config/config.php
@@ -0,0 +1,19 @@
+<?php
+
+
+//application
+$config['DEBUG_MODE'] = true;
+$config['BASE_URL'] = '/dev/inventix/api/';
+
+//database
+$config['DB_NAME'] = 'dbs5611232';
+$config['DB_USER'] = 'dbu772186';
+$config['DB_PASSWORD'] = 'yHjS6BfZV4dBDDh';
+$config['DB_HOST'] = 'db5006790823.hosting-data.io';
+
+//email
+$config['EMAIL_USERNAME'] = 'unsinn@foo.de';
+$config['EMAIL_PASSWORD'] = 'wirklich streng geheim';
+$config['EMAIL_SMTP'] = 'smtp.*****.de';
+$config['EMAIL_PORT'] = '587';
+
---
+++ doc/scrum/weekly_scrum_log.md
@@ -0,0 +1,19 @@
+# Weekly SCRUM
+
+## 2022-11-25 Ziele für 45-x Minuten
+
+### Artur
+
+- Gruppeneinteilung
+
+### Konrad, Ronja, Johanna, Béla, Belix
+
+- alte API recherchieren und analysieren
+
+### Tom, Justin, Gelix
+
+- altes ERM recherchieren und analysieren
+
+### Gelix
+
+- planlos
