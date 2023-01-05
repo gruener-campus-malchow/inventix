73fc08916ba942dd5975cc5607164d858f25041b
gk22_johanna <johannamariewagener@gmail.com>
2023-01-05 10:44:09 +0100

    insert query

---
+++ doc/db/insert_queries/johanna_inserts.sql
@@ -0,0 +1,63 @@
+INSERT INTO user
+VALUES (
+1,
+'Marie',
+'Müller',
+'Marienchenkäfer12',
+'mariemüller@gmail.com',
+'KJTI/&R34ZTFHG/(546$%HG8');
+
+INSERT INTO role
+VALUES (
+1,
+"Administrator",
+1,
+1,
+1);
+
+INSERT INTO locations 
+VALUES (
+'FG 2',
+'2.203',
+'Schrank rechts von der Tür, drittes rechtes Fach von oben');
+
+INSERT INTO description
+VALUES (
+1,
+'halt so ein Buch',
+2);
+
+INSERT INTO categories
+VALUES (
+1,
+'Fachbereich');
+
+
+INSERT INTO category_access
+VALUES (
+1,
+1);
+
+
+INSERT INTO assigned_role
+VALUES (
+1,
+1);
+
+INSERT INTO items
+VALUES (
+1,
+'Kompakt-Wissen Mathematik',
+'FG 2',
+'2.203',
+'Schrank rechts von der Tür, drittes rechtes Fach von oben',
+1);
+
+INSERT INTO tags
+VALUES ( 
+1,
+Mathe,
+'pending'
+1);
+
+
